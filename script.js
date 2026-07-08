3d08216d232360de5a03fda9ba84794b3e003af8
Valentina BM <valentina15bermo@gmail.com>
2026-07-07 21:49:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -27,6 +27,43 @@ const isUnder25 = n1 <= 25 && n2 <= 25 && n3<= 25 && n4 <=25;
 console.log(isUnder25);
 
 
+// part 2
+
+const distance = 1500;
+const budget = 175;
+const fuelCost = 3;
+
+const mpg55 = 30
+const mpg60 = 28;
+const mpg75 = 23;
+
+// how many gallons
+
+const gallons55 = distance / mpg55;
+const gallons60 = distance / mpg60;
+const gallons75 = distance / mpg75;
+
+console.log('At 55 mph, you will need ${gallons55} gallons of fuel');
+console.log('At 60 mph, you will need $}galoons60} gallons of fuel');
+console.log('At 70 mph, you will need ${gallons75} gallons of fuel');
+
+const cost55 = gallons55 * fuelCost;
+const cost60 = gallons60 * fuelCost;
+const cost75 = gallons75 * fuelCost;
+
+// budget
+
+ const budgetEnough55 = budget >= cost55;
+ const budgetEnough60 = budget >= cost60;
+ const budgetEnough75 = budget >= cost75;
+
+ console.log(`At 55 mph, is the budget enough? ${budgetEnough55}`);
+ console.log('AT 60 moh, is the budget enough? ${budgetEnough60}');
+ console.log('At 70 mph, is the budget enough? ${budgetEnough70}');
+
+
+
+
 // Check one: add up to 50
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
